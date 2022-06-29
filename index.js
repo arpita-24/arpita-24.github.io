3b67fe2de7e13fad1616c6f5bcf4c0fe29620112
Arpita Mohanty <89068505+arpita-24@users.noreply.github.com>
2022-06-30 01:33:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import App from './src/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
